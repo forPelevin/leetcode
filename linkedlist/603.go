b02669f9673b7d202b6b28856fc7989079d34c3d
Vlad Gukasov <forpelevin@gmail.com>
2020-05-15 11:29:42 +0300

    Delete k-th elementh from linked list

--- linkedlist/603.go
+++ linkedlist/603.go
@@ -19,4 +19,53 @@ package linkedlist
 //Could you do this in one pass?
 
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	if n < 1 {
+		return head
+	}
+
+	if n == 1 {
+		if head.Next == nil {
+			return nil
+		}
+
+		i := head
+		for i.Next.Next != nil {
+			i = i.Next
+		}
+		i.Next = nil
+		return head
+	}
+
+	i := head
+	k := head
+	j := 1
+	for i.Next != nil {
+		if j >= n {
+			k = k.Next
+		}
+		j++
+		i = i.Next
+	}
+
+	k.Val = k.Next.Val
+	k.Next = k.Next.Next
+
+	return head
+}
+
+func removeNthFromEndOptimized(head *ListNode, n int) *ListNode {
+	dummy := &ListNode{
+		Val:  0,
+		Next: head,
+	}
+	first, second := dummy, dummy
+	for i := 0; i <= n; i++ {
+		first = first.Next
+	}
+	for first != nil {
+		first = first.Next
+		second = second.Next
+	}
+	second.Next = second.Next.Next
+	return dummy.Next
 }
