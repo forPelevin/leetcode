7a09cb409f7d2920ba809aa090aad0545b537c1d
Vlad Gukasov <forpelevin@gmail.com>
2020-06-16 14:37:42 +0300

    Added a more optimal solution for the level order traversing

--- tree/628.go
+++ tree/628.go
@@ -16,6 +16,32 @@ package tree
 //[9,20],
 //[15,7]
 //]
+func levelOrderOptimized(root *TreeNode) [][]int {
+	res := make([][]int, 0)
+	if root == nil {
+		return res
+	}
+
+	queue := []*TreeNode{root}
+	for len(queue) > 0 {
+		qSize := len(queue)
+		currLvl := make([]int, qSize)
+		for i := 0; i < qSize; i++ {
+			node := queue[i]
+			currLvl[i] = node.Val
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+		queue = queue[qSize:]
+		res = append(res, currLvl)
+	}
+	return res
+}
+
 func levelOrder(root *TreeNode) [][]int {
 	return traverseNodesWithLevel(root, 0, make([][]int, 0, 100))
 }
